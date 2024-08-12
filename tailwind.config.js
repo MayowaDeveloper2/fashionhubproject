9d0a8c69c654e46ac66a566379be920735db7bc4
MayowaDeveloper2 <akintoyemayowa1@gmail.com>
2024-08-12 12:55:10 +0100

    work

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    
+    extend: {
+      colors: {
+        "customBlue" : "#05B9CA",
+        "customPurple" : "#6E4FA0",
+      },
+    },
+  },
+
+  plugins: [],
+}
